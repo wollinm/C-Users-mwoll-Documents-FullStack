ea6e6140e6da543b927a5fd3b05529b5645730d2
Magdalena Wollin <mwollino@hotmail.com>
2023-09-16 11:03:38 -0700

    init commit

---
+++ js/GuessNumber.js
@@ -0,0 +1,11 @@
+/* JavaScript
+Author: Magdalena Wollin
+Date: 09/16/2023
+*/
+//Ask user to guess number
+let num = prompt{"Guess a number between 1 and 10"};
+if (num == 5) {
+    alert {'You guessed the right number!'};
+} else {
+    alert {'Sorry, you guessed the wrong number'};
+}
