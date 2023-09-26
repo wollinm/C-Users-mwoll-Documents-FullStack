9a46f001c858ff23e687b1856e3d1b71a77756b6
Magdalena Wollin <mwollino@hotmail.com>
2023-09-25 20:45:29 -0700

    testing assignment 01

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 /* JavaScript
 Author: Magdalena Wollin
-Date: 09/16/2023
+Date: 09/25/2023
 */
-/*Ask user to guess number*/
-let num = prompt ('Guess a number between 1 and 10');
-if (num == 5) {
+/*Guessing number game*/
+let num = prompt ('Guess a number between 1 and 5');
+if (num == 3) {
     alert ('You guessed the right number!');
 } else {
     alert ('Sorry, you guessed the wrong number');
